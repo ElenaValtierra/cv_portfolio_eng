141eced9ceab034bb7cbb4ffe907ede17418d1c0
Elena Valtierra <elena.valt995@gmail.com>
2026-03-19 13:37:27 +0100

    Corrected error path on GitHub Pages using python

---
+++ script.py
@@ -0,0 +1,6 @@
+﻿from pathlib import Path
+p = Path(r'c:\Users\Elena\Documents\Portfolio\cv_portfolio_eng\index.html')
+text = p.read_text(encoding='utf-8')
+new = text.replace('href="/images/','href="images/').replace('src="/images/','src="images/').replace('url(/images/','url(images/').replace("href='/images/","href='images/").replace("src='/images/","src='images/").replace("url('/images/","url('images/")
+p.write_text(new, encoding='utf-8')
+print('ok')
